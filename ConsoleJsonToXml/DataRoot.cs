43f6d39710ef57d62bbd2372cb4086455abaf844
Aleksei Ogoltcov <aaogoltcov@mai.ru>
2025-04-29 20:32:25 +0300

    Revert "Коммит 3 (задание GIT)"
    This reverts commit ea07358c42c29580344db73472fd257ec9f6bd49.

--- ConsoleJsonToXml/DataRoot.cs
+++ ConsoleJsonToXml/DataRoot.cs
@@ -7,6 +7,8 @@ public class DataRoot
 {
     [XmlArray("Data.Root.Names")] [XmlArrayItem("Name")]
     public string[]? Names;
+
     [XmlElement("Data.Entry")] public DataEntry? Entry;
+
     [XmlElement("Data#ExtendedEntry")] public DataExtendedEntry? ExtendedEntry;
 }
