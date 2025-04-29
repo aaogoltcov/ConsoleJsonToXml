ea07358c42c29580344db73472fd257ec9f6bd49
Aleksei Ogoltcov <aaogoltcov@mai.ru>
2025-04-29 20:32:10 +0300

    Коммит 3 (задание GIT)

--- ConsoleJsonToXml/DataRoot.cs
+++ ConsoleJsonToXml/DataRoot.cs
@@ -7,8 +7,6 @@ public class DataRoot
 {
     [XmlArray("Data.Root.Names")] [XmlArrayItem("Name")]
     public string[]? Names;
-
     [XmlElement("Data.Entry")] public DataEntry? Entry;
-
     [XmlElement("Data#ExtendedEntry")] public DataExtendedEntry? ExtendedEntry;
 }
